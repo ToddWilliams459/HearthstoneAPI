3a73d1f9f8591c9eaa1cad8995bfc508188eb757
ToddWilliams459 <tkw8341@rit.edu>
2017-10-11 16:50:58 -0400

    Stubbed API Manager

---
+++ src/ApiManager.js
@@ -0,0 +1,24 @@
+class ApiManager{
+	constructor (baseUrl, key){
+		this.baseUrl = baseUrl;
+		this.key = key;
+	}
+
+	function get(uri){
+		//
+	}
+	
+	function post(uri){
+	
+	}
+	
+	function head(uri){
+	
+	}
+	
+}
+
+/*
+var api = new ApiManager("pokeapi.co", "fasdfsadfsad");
+var bulbasaur = api.get("pokemon/bulbasaur");
+*/
